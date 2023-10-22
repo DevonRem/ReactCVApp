0ae39a499f6af95b8b9da14b4248ebbbd9654875
DevonRem <DevonReminick@gmail.com>
2023-10-21 21:10:32 -0400

    Create first component

--- ReactCVApp/src/App.jsx
+++ ReactCVApp/src/App.jsx
@@ -1,11 +1,12 @@
 //import { useState } from 'react'
 import './App.css'
+import GeneralInfo from './components/GeneralInfo'
 //focus on making these into components after
 function App() {
 
   return (
     <>
-
+      <GeneralInfo></GeneralInfo>
     </>
   )
 }
--- ReactCVApp/src/components/GeneralInfo.jsx
+++ ReactCVApp/src/components/GeneralInfo.jsx
@@ -1,11 +1,25 @@
 //import { useState } from 'react'
 import '../styles/GeneralInfo.css'
 
+
 function GeneralInfo() {
+//    const [inputs, setInputs] = useState({});
 
     return (
       <>
-  
+        <div className="GeneralInfo">
+        <h2>General Information</h2>
+           <form className='formName'>
+                <div className='nameSection'>
+                    <input type='text' className='firstName' placeholder='First Name' maxLength='15'/>
+                    <input type='text' className='lastName' placeholder='Last Name' maxLength='15'/>
+                </div>
+                <div className='theRest'>
+                    <input type='text' placeholder='Email' maxLength='80'/>
+                    <input type='text' placeholder='Phone Number' maxLength='80'/>
+                </div>
+           </form>
+        </div>
       </>
     )
   }
