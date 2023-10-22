3327eeb91465fba39c5e343594dcb7e107dadd31
DevonRem <DevonReminick@gmail.com>
2023-10-21 20:19:20 -0400

    Set up react and general files/folders

---
+++ ReactCVApp/src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './App.css'
+//focus on making these into components after
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ ReactCVApp/src/components/GeneralInfo.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import '../styles/GeneralInfo.css'
+
+function GeneralInfo() {
+
+    return (
+      <>
+  
+      </>
+    )
+  }
+  
+  export default GeneralInfo
